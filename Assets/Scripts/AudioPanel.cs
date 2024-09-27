e16f9949c4e121a0f6dc9b6661b02fd087813f3c
Dnk095 <darcnikill@gmail.com>
2024-09-27 21:32:45 +1200

    Update AudioPanel.cs

--- Assets/Scripts/AudioPanel.cs
+++ Assets/Scripts/AudioPanel.cs
@@ -1,34 +1,38 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioPanel : MonoBehaviour
 {
+    private const string MasterVolume = nameof(MasterVolume);
+    private const string SoundVolume = nameof(SoundVolume);
+    private const string BackGroundVolume = nameof(BackGroundVolume);
 
     [SerializeField] private AudioMixerGroup _mixer;
+    [SerializeField] private VolumeChanger _soundSlider;
+    [SerializeField] private VolumeChanger _masterSlider;
+    [SerializeField] private VolumeChanger _backGroundSlider;
 
-    private bool _isMute = false;
+    private void OnEnable()
+    {
+        _soundSlider.OnVolumeChange += ChangeVolume;
+        _masterSlider.OnVolumeChange += ChangeVolume;
+        _backGroundSlider.OnVolumeChange += ChangeVolume;
+    }
 
     public void Mute(bool enabled)
     {
         if (enabled)
-            _mixer.audioMixer.SetFloat("MasterVolume", 0);
+            _mixer.audioMixer.SetFloat(MasterVolume, 0);
         else
-            _mixer.audioMixer.SetFloat("MasterVolume", -80);
+            _mixer.audioMixer.SetFloat(MasterVolume, -80);
     }
 
-
-    public void ChangeSoundValue(float volume)
+    private void ChangeVolume(string name, float volume)
     {
-        _mixer.audioMixer.SetFloat("SoundVolume", Mathf.Lerp(-80, 0, volume));
-    }
-
-    public void ChangeBackGroundVolume(float volume)
-    {
-        _mixer.audioMixer.SetFloat("BackGroundVolume", Mathf.Lerp(-80, 0, volume));
-    }
-
-    public void ChangeMasterVolume(float volume)
-    {
-        _mixer.audioMixer.SetFloat("MasterVolume", Mathf.Lerp(-80, 0, volume));
+        if (volume <= 0.02)
+            _mixer.audioMixer.SetFloat(name, -80);
+        else
+            _mixer.audioMixer.SetFloat(name, Mathf.Log10(volume) * 20);
     }
 }
---
+++ Assets/Scripts/VolumeChanger.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeChanger : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private string _sliderName;
+
+    public event Action<string,float> OnVolumeChange;
+
+    private void OnEnable()
+    {
+        _slider.onValueChanged.AddListener(ChangeVolume);
+    }
+
+    private void OnDisable()
+    {
+        _slider.onValueChanged.RemoveListener(ChangeVolume);
+    }
+
+    private void ChangeVolume(float volume)
+    {
+        OnVolumeChange?.Invoke(_sliderName, volume);
+    }
+}
